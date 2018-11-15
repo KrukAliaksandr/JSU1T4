b9c169e2bd4d1f1adee9d72a3f082c5ce9088a73
Aliaksandr Kruk <Aliaksandr_Kruk@epam.com>
2018-11-15 19:22:47 +0300

    add promise to the function

--- task1.js
+++ task1.js
@@ -1,11 +1,18 @@
-import { XMLHttpRequest as xml } from "xmlhttprequest";
+// import { XMLHttpRequest as xml } from "xmlhttprequest";
+let xml = require('xmlhttprequest').XMLHttpRequest;
 let xhr = new xml();
-xhr.open('GET', 'http://services.groupkt.com/country/get/all', false);
+xhr.open('GET', 'http://services.groupkt.com/country/get/all', true);
 xhr.send();
+function getText(){return new Promise((resolve,reject)=>{xhr.onreadystatechange = function() {
+  if (xhr.readyState != 4) return;
 if (xhr.status != 200) {
-  console.log(xhr.status + ': ' + xhr.statusText);
+  reject(new Errror(xhr.status + ': ' + xhr.statusText));
 } else {
-  console.log(xhr.responseText);
-  let js = JSON.parse(xhr.responseText).RestResponse.result
-  console.log(js[3])
+  resolve(xhr.responseText);
 }
+}})};
+getText().then((text)=>{
+  let js = JSON.parse(text).RestResponse.result
+  console.log(js[3])
+}).catch()
+ 
