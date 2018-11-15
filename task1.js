c057aee54d0716b64c0abd8a5a04e918698ae2c8
Aliaksandr Kruk <Aliaksandr_Kruk@epam.com>
2018-11-15 18:12:14 +0300

    remove unnecessary commentaries in code

--- task1.js
+++ task1.js
@@ -1,19 +1,11 @@
-let xml = require("xmlhttprequest").XMLHttpRequest
-let xhr = new xml()
-
-// 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-xhr.open('GET','http://services.groupkt.com/country/get/all', false);
-
-// 3. Отсылаем запрос
+import { XMLHttpRequest as xml } from "xmlhttprequest";
+let xhr = new xml();
+xhr.open('GET', 'http://services.groupkt.com/country/get/all', false);
 xhr.send();
-
-// 4. Если код ответа сервера не 200, то это ошибка
 if (xhr.status != 200) {
-  // обработать ошибку
-  console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+  console.log(xhr.status + ': ' + xhr.statusText);
 } else {
-  // вывести результат
-  console.log( xhr.responseText ); // responseText -- текст ответа.
+  console.log(xhr.responseText);
   let js = JSON.parse(xhr.responseText).RestResponse.result
   console.log(js[3])
 }
